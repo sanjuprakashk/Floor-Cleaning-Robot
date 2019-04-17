777cb6871b0293b949a0f64795941cf2fc4bc029
Sanju Prakash Kannioth <Sanju.Kannioth@colorado.edu>
2019-04-17 01:13:35 -0600

    Added folder structure and test uart code for BBG

---
+++ BBG/uartconfig.sh
@@ -0,0 +1,11 @@
+echo "\n Pin configuration available"
+    echo "\n UART 4 configuration p9.11 and p9.13"
+    sudo config-pin P9.11 uart
+    sudo config-pin -q P9.11
+    sudo config-pin P9.13 uart
+    sudo config-pin -q P9.13
+    echo "\n UART 1 configuration p9.26 and p9.24"
+    sudo config-pin P9.24 uart
+    sudo config-pin -q P9.24
+    sudo config-pin P9.26 uart
+    sudo config-pin -q P9.26
